a826088e2b85e469e4e7bbc015908958069dd0c2
thevioletmaniac <thevioletmaniac@gmail.com>
2021-04-24 12:14:00 +0600

    Add reducers: adding/removing item, deleting all items

--- src/store/index.js
+++ src/store/index.js
@@ -3,25 +3,46 @@ import { createStoreon } from 'storeon';
 
 const defaultTodos = [
   {
-    id: 1,
-    todo: 'Do something',
-    isCompleted: false,
+    title: `Don't worry`,
+    isCompleted: true,
   },
   {
-    id: 2,
-    todo: 'Do something else',
-    isCompleted: true,
+    title: 'Be happy',
+    isCompleted: false,
   },
 ];
 
+const DEFAULT_TODO_ID = 0;
+
 const todos = (store) => {
-  store.on('@init', () => ({ todos: defaultTodos }));
-  store.on('todos/setTodo', ({ todos }, obj) => {
+  store.on('@init', () => ({ id: DEFAULT_TODO_ID, todos: defaultTodos }));
+
+  store.on('todos/addNewTodo', ({ todos, id }, newTodo) => {
     return {
-      todos: {
-        ...todos,
-        todos: obj,
-      },
+      id: id + 1,
+      todos: todos.concat([
+        {
+          id: id,
+          title: newTodo,
+          isCompleted: false,
+        },
+      ]),
+    };
+  });
+
+  store.on('todos/deleteTodo', ({ todos }, todoId) => {
+    var updatedTodos = todos.filter((todo) => {
+      return todo.id !== todoId;
+    });
+    return {
+      todos: updatedTodos,
+    };
+  });
+
+  store.on('todos/deleteAllTodos', () => {
+    return {
+      id: DEFAULT_TODO_ID,
+      todos: [],
     };
   });
 };
