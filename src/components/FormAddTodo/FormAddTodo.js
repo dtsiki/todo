544dd8b8296074d1ce4435fec33465f7ae54f444
thevioletmaniac <thevioletmaniac@gmail.com>
2021-04-27 22:27:49 +0600

    Split adding to do form to component

---
+++ src/components/FormAddTodo/FormAddTodo.js
@@ -0,0 +1,50 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { useStoreon } from 'storeon/react';
+
+import Button from './../../components/Button';
+import Form from './../../components/Form';
+import Input from './../../components/Input';
+
+import './style.scss';
+
+const FormAddTodo = () => {
+  const { dispatch } = useStoreon();
+  const [newTodo, setNewTodo] = useState('');
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    if (!newTodo) return;
+    dispatch('todos/addNewTodo', newTodo);
+    setNewTodo('');
+  };
+
+  const changeNewTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  return (
+    <Form onSubmit={addNewTodo}>
+      <Input
+        value={newTodo}
+        onChange={changeNewTodo}
+        type="search"
+        name="search"
+        id="addNewTodo"
+        placeholder="e.g: don't worry and be happy"
+        label="Add new to do"
+      />
+      <Button
+        buttonClassName="add-button"
+        isValueVisuallyHidden={true}
+        icon={<FontAwesomeIcon icon={faPlus} />}
+        value="Add"
+        onClick={addNewTodo}
+        type="submit"
+      />
+    </Form>
+  );
+};
+
+export default FormAddTodo;
